0bfb4d7510c53f87608546e80afca2b705979d73
Zero <samuelbustos10903@gmail.com>
2023-05-19 21:41:52 -0300

    feat: footer added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,35 @@
+<script setup></script>
 <template>
 	<footer class="footer">
-		<p>Testing footer</p>
+		<article class="footer__contact">
+			<h2 class="footer__title">Contáctanos</h2>
+			<a href="http://instagram.com/wickedness__delirium" target="_blank"
+				>Instagram</a
+			>
+			<a href="mailto:sam.alejandro.bustos@gmail.com" target="_blank">Gmail</a>
+			<a href="http://github.com/Zero10903" target="_blank">Github</a>
+		</article>
 	</footer>
 </template>
 
-<script setup></script>
+<style lang="scss" scoped>
+@use '../assets/sass/02-tools/effect';
+@use '../assets/sass/02-tools/layout';
+@import '../assets/sass/01-settings/settings';
+.footer {
+	margin: 0.5em;
+	border: $box-border;
+	border-radius: $box-radius;
+	@include effect.glass($box-blur);
+
+	&__contact {
+		padding: 1em;
+		@include layout.flex(column, wrap, center, center, 0.5em);
+	}
+	&__title {
+		padding-bottom: 0.5em;
+		font-size: 1.5em;
+		text-align: center;
+	}
+}
+</style>
